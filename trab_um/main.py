0793d3b27fa1b32ac156d34125277ede27398315
Caio Silva <vinczken@gmail.com>
2025-04-20 21:00:46 -0300

    terminado o codigo

--- trab_um/main.py
+++ trab_um/main.py
@@ -16,7 +16,7 @@ with open("output.txt", "w") as output:
             x = []
             y = []
             A, b = gerar_sistema(ns[i])
-            
+
             x.append(eliminacao_gauss_sem_pivoteamento(A, b))
             y.append(np.linalg.solve(A, b))
             output.write(f"A solucao de m{ns[i]}, n{j + 1}:\n")
@@ -40,7 +40,6 @@ with open("output.txt", "w") as output:
                 output.write(f"Jacobi, iter: {iteracoes_j},\tGauss_Seidel, iter: {iteracoes_gs}\n\n\n")
             output.write(f"\n")
 
-
     output.write(f"\n\nLagrange & Newton\n\n")
     x = symbols('x')
     funcao = sin(2 * np.pi * x) + 0.2 * cos(4 * np.pi * x) + 0.1 * x
@@ -48,56 +47,87 @@ with open("output.txt", "w") as output:
     n_pontos = [10, 20, 15]
 
     pontos_totais = []
+
     xs_totais = []
     ys_totais = []
 
     pontos_uniforme = np.linspace(0.0, 1.0, n_pontos[0])
     pontos_uniforme = [ round(float(p), 4) for p in pontos_uniforme]
-    pontos_totais.append([
+    pontos_totais.extend(
         (p, round(funcao.subs(x, p), 4))
         for p in pontos_uniforme
-    ])
-    xs_totais.append(pontos_uniforme)
-    ys_totais.append([round(funcao.subs(x, p), 4) for p in pontos_uniforme])
-
+    )
+    xs_totais.extend(pontos_uniforme)
+    ys_totais.extend(round(funcao.subs(x, p), 4) for p in pontos_uniforme)
 
     pontos_uniforme = np.linspace(0.0, 1.0, n_pontos[1])
     pontos_uniforme = [ round(float(p), 4) for p in pontos_uniforme]
-    pontos_totais.append([
+    pontos_totais.extend(
         (p, round(funcao.subs(x, p), 4))
         for p in pontos_uniforme
-    ])
-    xs_totais.append(pontos_uniforme)
-    ys_totais.append([round(funcao.subs(x, p), 4) for p in pontos_uniforme])
+    )
+    xs_totais.extend(pontos_uniforme)
+    ys_totais.extend(round(funcao.subs(x, p), 4) for p in pontos_uniforme)
 
     pontos_aleatorios = gerar_pontos_baseado_funcao(n_pontos[2], funcao)
-    pontos_totais.append(pontos_aleatorios)
-    xs_totais.append([p[0] for p in pontos_aleatorios])
-    ys_totais.append([round(funcao.subs(x, p[1]), 4) for p in pontos_aleatorios])
+    pontos_totais.extend(pontos_aleatorios)
+    xs_totais.extend(p[0] for p in pontos_aleatorios)
+    ys_totais.extend(round(funcao.subs(x, p[1]), 4) for p in pontos_aleatorios)
 
     p_x_lagrange = []
     p_x_newton = []
     xs_inter_lagrange = []
     xs_inter_newton = []
 
+    print(xs_totais)
+    print(ys_totais)
     for i in range(0, len(pontos_totais)):
-        for j in range(0, len(pontos_totais[i])):
-            output.write(f"\nPontos [{i}, {j}]: {pontos_totais[i][j]}\n")
-        output.write(f"\n")
-        p_x_lagrange_temp, xs_inter_temp = metodo_lagrange(pontos_totais[i], xs_inter = xs_totais[i])
-        p_x_lagrange.append(p_x_lagrange_temp)
-        xs_inter_lagrange.append(xs_inter_temp)
-        
-        p_x_newton_temp, xs_inter_temp = metodo_newton(xs_totais[i], ys_totais[i], xs_inter = xs_totais[i])
-        p_x_newton.append(p_x_newton_temp)
-        xs_inter_newton.append(xs_inter_temp)
+        output.write(f"Pontos [{i}]: {pontos_totais[i]}\n")
+
+    p_x_lagrange_temp, xs_inter_temp = metodo_lagrange(pontos_totais, xs_inter = xs_totais)
+    p_x_lagrange.append(p_x_lagrange_temp)
+    xs_inter_lagrange.extend(xs_inter_temp)
+
+    p_x_newton_temp, xs_inter_temp = metodo_newton(xs_totais, ys_totais, xs_inter = xs_totais)
+    p_x_newton.append(p_x_newton_temp)
+    xs_inter_newton.extend(xs_inter_temp)
     output.write(f"\n\n")
 
     output.write(f"Equacao original: {funcao}\n")
-    for i in range(0, len(pontos_totais)):
-        output.write(f"\nLagrange:\nEquacao P(x): {p_x_lagrange[i]}\n\n")
-        for j in range(0, len(xs_inter_lagrange[i])):
-            output.write(f"P({xs_totais[i][j]}): {round(xs_inter_lagrange[i][j], 4)},\tF_original: {pontos_totais[i][j][1]},\tErro: {abs(round(xs_inter_lagrange[i][j], 4) - pontos_totais[i][j][1])}\n")
-        output.write(f"\nNewton:\nEquacao P(x): {p_x_newton[i]}\n\n")
-        output.write(f"P({xs_totais[i][j]}): {round(xs_inter_newton[i][j], 4)},\tF_original: {pontos_totais[i][j][1]},\tErro: {abs(round(xs_inter_newton[i][j], 4) - pontos_totais[i][j][1])}\n")
-        output.write("\n")
+    output.write(f"\nLagrange:\nEquacao P(x): {p_x_lagrange}\n\n")
+    output.write(f"\nNewton:\nEquacao P(x): {p_x_newton}\n\n")
+
+    for j in range(0, len(xs_inter_lagrange)):
+        output.write(f"P_L({xs_totais[j]}): {round(xs_inter_lagrange[j], 4)},\tF_original: {pontos_totais[j][1]},\tErro: {abs(round(xs_inter_lagrange[j] - pontos_totais[j][1], 4))}\n")
+    output.write("\n\n\n")
+    for j in range(0, len(xs_inter_newton)):
+        output.write(f"P_N({xs_totais[j]}): {round(xs_inter_newton[j], 4)},\tF_original: {pontos_totais[j][1]},\tErro: {abs(round(xs_inter_newton[j] - pontos_totais[j][1], 4))}\n")
+    output.write("\n")
+
+
+    output.write("\n\nInterpolacao e analise\n\n")
+    xs_para_interpolar = np.linspace(0.0, 1.0, 1000)
+    xs_para_interpolar = [ round(float(p), 4) for p in xs_para_interpolar]
+
+    ys_depois_interpolar_Real = []
+    ys_depois_interpolar_L = []
+    ys_depois_interpolar_N = []
+
+    ys_depois_interpolar_Real.extend((funcao.subs(x, xs_para_interpolar[i]) for i in range(0, len(xs_para_interpolar))))
+    ys_depois_interpolar_L.extend((p_x_lagrange[0].subs(x, xs_para_interpolar[i]) for i in range(0, len(xs_para_interpolar))))
+    ys_depois_interpolar_N.extend((p_x_newton[0].subs(x, xs_para_interpolar[i]) for i in range(0, len(xs_para_interpolar))))
+
+    erros_abs_L = []
+    erros_abs_N = []
+
+    for i in range(0, len(xs_para_interpolar)):
+        output.write(f"P_L({xs_para_interpolar[i]}): {round(ys_depois_interpolar_L[i], 4)},\tP_N({xs_para_interpolar[i]}): {round(ys_depois_interpolar_N[i], 4)},\tP_Original({xs_para_interpolar[i]}): {round(ys_depois_interpolar_Real[i], 4)}\n")
+        output.write(f"Erro_L: {abs(round(ys_depois_interpolar_L[i] - ys_depois_interpolar_Real[i], 4))},\tErro_N: {abs(ys_depois_interpolar_N[i] - ys_depois_interpolar_Real[i])}\n\n")
+        erros_abs_L.append(abs(round(ys_depois_interpolar_L[i] - ys_depois_interpolar_Real[i], 4)))
+        erros_abs_N.append(abs(round(ys_depois_interpolar_N[i] - ys_depois_interpolar_Real[i], 4)))
+
+    output.write(f"\n\n\nErro maximo Lagrange: {max(erros_abs_L)}\n")
+    output.write(f"Erro maximo Newton: {max(erros_abs_N)}\n\n")
+
+    output.write(f"Erro medio Lagrange: {round(sum(erros_abs_L)/len(xs_para_interpolar), 4)}\n")
+    output.write(f"Erro medio Newton: {round(sum(erros_abs_N)/len(xs_para_interpolar), 4)}\n")
--- trab_um/newton.py
+++ trab_um/newton.py
@@ -1,50 +1,36 @@
-import numpy as np
 from sympy import symbols, Mul, expand, Eq
 from sympy.printing import sstr
-import copy
 
-def diferencas_divididas(xs, ys, f_x):
-    n = len(f_x)
-    if 3 > n:
-        if n == 1:
-            index = xs.index(f_x[0])
-            return ys[index]
-        if n == 2:
-            x0 = xs.index(f_x[0])
-            x1 = xs.index(f_x[1])
-            divisor = (x1 - x0)
-            if divisor == 0:
-                divisor = 0.01
-            return ((ys[x1] - ys[x0])/divisor)
-    else:
-        diferenca = diferencas_divididas(xs, ys, f_x[1:])
-        dividida = diferencas_divididas(xs, ys, f_x[:-1])
-        divisor = (f_x[n - 1] - f_x[0])
-        if divisor == 0:
-            divisor = 0.01
-        return (diferenca - dividida) / divisor
+def tabela_diferencas_divididas(xs, ys):
+    n = len(xs)
+    tabela = [ys.copy()]
+
+    for i in range(1, n):
+        linha_anterior = tabela[-1]
+        nova_linha = []
+        for j in range(n - i):
+            numerador = linha_anterior[j+1] - linha_anterior[j]
+            denominador = xs[j+i] - xs[j]
+            if denominador == 0:
+                denominador = 0.01  # evitar divisão por zero
+            nova_linha.append(numerador / denominador)
+        tabela.append(nova_linha)
+    
+    return [linha[0] for linha in tabela]  # retornamos apenas os coeficientes
+
 
 def metodo_newton(xs, ys, xs_inter):
     x = symbols('x')
-    p_x = 0
+    coeficientes = tabela_diferencas_divididas(xs, ys)
+    p_x = coeficientes[0]
     
-    fs = []
-    for i in range(1, len(xs) + 1):
-        valores = xs[0:i]
-        f_atual = diferencas_divididas(xs, ys, valores)
-        fs.append(f_atual)
+    for i in range(1, len(coeficientes)):
+        termo = 1
+        for j in range(i):
+            termo *= (x - xs[j])
+        p_x += coeficientes[i] * termo
     
-    p_x += fs[0]
-    
-    i = 1
-    for j in range(1, len(fs)):
-        f_x = fs[i]
-        equacao = 1
-        for k in range(0, j):
-            expressao = (x - xs[k])
-            equacao *= expressao
-        p_x += f_x * equacao
-        i += 1
-    
-    pontos_inter = [ p_x.subs(x, xs_inter[i]) for i in range(len(xs_inter)) ]
+    pontos_inter = [p_x.subs(x, val) for val in xs_inter]
     return expand(p_x), pontos_inter
+
+
